46d0c7baf8cb6090e4e9991fc7f34bc54e5f6a4f
ArmorDude <legend.ivanov@gmail.com>
2025-04-06 22:29:15 +0300

    Create project basic structure, setup environment folder and initialize Firebase

---
+++ React Project SoftUni Course 2025/src/environments/environment.example.js
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "type provided api key here",
+        authDomain: "boardgamehub-4a607.firebaseapp.com",
+        projectId: "boardgamehub-4a607",
+        storageBucket: "boardgamehub-4a607.firebasestorage.app",
+        messagingSenderId: "190755874991",
+        appId: "1:190755874991:web:eb0bf26547ee3067584c5a"
+    }
+};
---
+++ React Project SoftUni Course 2025/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { environment } from './environments/environment.development';
+
+const app = initializeApp(environment.firebase);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
